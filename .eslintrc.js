c51e23f050e93caeabe3351318382415dcfab8c1
Igor Luíz <igor@devstacks.com.br>
2016-08-31 22:19:08 -0300

    Change to airbnb on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
+  extends: 'airbnb',
   plugins: [
     'html'
   ],
