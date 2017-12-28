6010f7be8232b4e6f815c0a597b66ac346db94fa
Igor Felipe <igor_play_22@hotmail.com>
2017-12-28 13:51:57 -0200

    Service Worker OK

---
+++ app.js
@@ -0,0 +1,9 @@
+(() => {
+
+  if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
+  }
+
+})();
---
+++ sw.js
@@ -0,0 +1,40 @@
+const cacheName = 'vnote-v1';
+const dataCacheName = 'vnote-final-v1';
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll([
+          '/'
+          , 'manifest.json'
+          , '/index.html'
+          , 'app.js'
+          , '/dist/build.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
